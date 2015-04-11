8505f165a18d834b015b57dd2d2a76ad94a9e3e9
Milan <milan.dicic@gmail.com>
2015-04-11 15:34:17 +0200

    front page test and manu slider

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Turisticki vodic</title>
+
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+
+	<div data-role="page" id="page1">
+		<div data-role="panel" id="menu" data-position="left" data-display="push">
+			<ul>
+				<li>Home</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+
+		<div data-role="header">
+			<a href="#menu" data-icon="bars">Menu</a>
+			<h1>Turisticki vodic</h1>
+		</div>
+
+		<div data-role="main" class="ui-content">
+			<h3>Prva strana main content</h3>
+			<p>Aplikacija turisticki vodic test podaci bla, bla</p>
+		</div>
+
+		<!-- <div data-role="footer">
+			<h4>&copy; Milan Dicic and Stefan Jankovic, Nis 2015</h4>
+		</div> -->
+	</div>
+</body>
+</html>
